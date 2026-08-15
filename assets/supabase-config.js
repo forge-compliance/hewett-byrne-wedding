92518ee2adbec0e84571ed2791b50104853f494e
forge-compliance <gphewett@gmail.com>
2026-08-15 21:59:47 +0100

    Add files via upload

---
+++ assets/supabase-config.js
@@ -0,0 +1,3 @@
+const WEDDING_SUPABASE_URL = 'https://vyvsouqgzyobxfjuwctl.supabase.co';
+const WEDDING_SUPABASE_KEY = 'sb_publishable_PXSxzF2prI_81L4_w_ypLw_te6yJl8q';
+const weddingSupabase = window.supabase.createClient(WEDDING_SUPABASE_URL, WEDDING_SUPABASE_KEY);
