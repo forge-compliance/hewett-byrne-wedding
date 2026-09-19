44800af174064a3699179db2242c6f715e4b3ee5
forge-compliance <gphewett@gmail.com>
2026-09-19 20:56:16 +0100

    Add hidden game service worker

---
+++ hidden-game/sw.js
@@ -0,0 +1,1 @@
+const C='jo-game-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./game.js','./manifest.json']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
